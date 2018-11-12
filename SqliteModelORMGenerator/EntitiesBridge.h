ae233ff6c256f1092763c572ab8a2e45519d674b
“xiaoke” <xiaoke@cqssmail.com>
2018-11-12 14:27:27 +0800

    Release 0.0.1.

---
+++ SqliteModelORMGenerator/EntitiesBridge.h
@@ -0,0 +1,16 @@
+//
+//  EntitiesBridge.h
+//  Psy-TeachersTerminal
+//
+//  Created by apple on 2016/7/6.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#ifndef EntitiesBridge_h
+#define EntitiesBridge_h
+
+#define EntityNames @"ShoppingCarProducts\nAppPayRecordModel\nAppVersionCheckModel"
+#import "ShoppingCarProducts.h"
+#import "AppPayRecordModel.h"
+#import "AppVersionCheckModel.h"
+#endif /* EntitiesBridge_h */
